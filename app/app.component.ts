2f5462a9255a014e9075627c61b65b6a6e72dc40
Anderson Nascimento <and.pax@gmail.com>
2016-09-28 23:57:23 -0300

    Template de projeto Pet Mais Faculdade Nova Roma Cadeira Projetos II

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by andypax on 28/09/16.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Pet Mais - Passeios para cães</h1>'
+})
+export class AppComponent { }
